4c27516aa1b418665309ee99a6a9219dbe32246a
rigolook <rixxormen@gmail.com>
2023-10-06 10:57:59 -0700

    YA

---
+++ ExamenR_EESA/Datos/Datos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenR_EESA.Datos
+{
+    internal class Datos
+    {
+        //#############################################################
+        //No alcancé
+        /*public void Fcurp()
+        {
+            int ai = 4; int af = 6;
+            int mi = 6; int mf = 8;
+            int di = 8; int df = 10;
+            int detectori = 10;
+            int detectorf = 11;
+            Console.WriteLine("Dame tu CURP");
+            string curp = Console.ReadLine();
+            string dia = curp.Substring(di, df);
+            string mes = curp.Substring(mi, mf);
+            string año = curp.Substring(ai, af);
+            string detector = curp.Substring(detectori, detectorf);
+            Console.WriteLine($"Tu fecha de nacimiento es {dia}/{mes}/{año}");
+            switch( detector )
+            {
+                case "H":
+                    Console.WriteLine("Eres Hombre"); break;
+                case "M":
+                    Console.WriteLine("Eres Mujer"); break;
+            }
+        }*/
+        //############################################################
+        //TERMINADO-----------------------------------------------------------------
+        /*public void Intervalo(int inicio, int fin)
+        {
+            Console.WriteLine("\nEl intervalo de estos numeros sería: \n");
+            while (inicio <= fin)
+            {
+                Console.WriteLine($"{inicio}");
+                inicio++;
+            }
+        }*/
+        //###########################################################
+        //TERMINADO---------------------------------------------------------------
+        /*public void Tabla(int numero)
+        {
+            int resultado;
+            int contador = 0;
+            Console.WriteLine("\nLa tabla de multiplicar del numero dado es: ");
+            while (contador < 10)
+            {
+                contador++;
+                resultado = numero * contador;
+                Console.WriteLine($"{numero} x {contador} = {resultado}");
+            }
+
+
+
+        }*/
+        //###########################################################
+    }
+}
--- ExamenR_EESA/Program.cs
+++ ExamenR_EESA/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Datos.Datos datos = new Datos.Datos();
+            //######## EJERCICIO CURP ##################################
+            datos.Fcurp();
+            //##########################################################
+            
+            //######## EJERCICIO INTERVALO #############################
+            /*
+            Console.WriteLine("Vamos a sacar un intervalo, desde los numeros que tu proporciones!");
+            Console.WriteLine("Dame un numero: ");
+            int inicio = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame otro numero: ");
+            int fin = Convert.ToInt32(Console.ReadLine());
+            datos.Intervalo(inicio, fin);*/
+            //#########################################################
+
+            //######## EJERCICIO TABLA DE MULTIPLICAR #################
+            /*Console.WriteLine("Dame un numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            datos.Tabla(numero);
+            */
+            //#########################################################
         }
     }
 }
